178736fa2125271ba21d204081b5a4a7289b62ea
Ilya Nechaev <dolphins.mind777@gmail.com>
2021-09-22 12:50:22 +0300

    ded mode;

---
+++ AnimationApp/Models/Animation.swift
@@ -0,0 +1,19 @@
+//
+//  Animation.swift
+//  AnimationApp
+//
+//  Created by Илья Нечаев on 22.09.2021.
+//
+
+import Foundation
+import UIKit
+
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: CGFloat
+    let duration: CGFloat
+    let delay: CGFloat
+}
+
+
